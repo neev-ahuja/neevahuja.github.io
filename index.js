83bd1304c138a125adb1de484386f9bd1747dfa5
neev-ahuja <neevahuja88@gmail.com>
2024-10-19 22:26:03 +0530

    Responsive Code Added

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 document.getElementById("menu").onclick = ()=>{
-    document.getElementById("ul").display = "block";
+    document.getElementById("menu").style.display = "none";
+    document.getElementById("close").style.display = "block";
+    document.getElementById("ul").style.display = "block";
+    document.getElementById("menuCont").style.borderRight = "#43afa1 solid 4px";
+    document.getElementById("menuCont").style.width = "50vw";
+    document.getElementById("menuCont").style.animation = "menuAnimStart 0.3s";
+}
+document.getElementById("close").onclick = ()=>{
+    document.getElementById("menu").style.display = "block";
+    document.getElementById("close").style.display = "none";
+    document.getElementById("ul").style.display = "none";
+    document.getElementById("menuCont").style.width = "fit-content";
+    document.getElementById("menuCont").style.borderRight = "none";
+    document.getElementById("menuCont").style.animation = "menuAnimEnd 0.3s";
 }
